cfe91aad12ed3f9c8b14bcd34538a9e13fc83856
iiiiiii1 <25471406+iiiiiii1@users.noreply.github.com>
2018-05-11 19:51:34 +0800

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -64,6 +64,7 @@ start_socat(){
         chmod +x /etc/rc.d/rc.local
     else
     source /etc/os-release &>/dev/null
+    if [[ "${ID}" == "debian" && ${VERSION_ID} -ge 9 ]];then
     echo -e "${Green}检测到 Debian${VERSION_ID} 无/etc/rc.local自启，正在为其配置... ${Font} "
     elif [[ "${ID}" == "ubuntu" && `echo "${VERSION_ID}" | cut -d '.' -f1` -ge 18 ]];then
     echo -e "${Green}检测到 Ubuntu${VERSION_ID} 无/etc/rc.local自启，正在为其配置... ${Font} "
